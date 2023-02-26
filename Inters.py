39c548c2658d7530e7ffdcf88777fb999f0a9293
FabianTrapman <fabiantrapmanleander@hotmail.com>
2023-02-26 20:30:17 +0100

    Veel toegevoegd, heb niet veel gelet op kleine commits, dus dit is een flinke hap

--- Inters.py
+++ Inters.py
@@ -30,7 +30,45 @@ def answer():
 
     return list_colors
 
-def human_question(question_list, answer_code):
+def answer_npc():
+
+    colors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
+
+    answer_game = []
+
+    print('A code consists of four colors.\n'
+          'Colors to choose from: blue, green, orange, purple, red, yellow\n')
+
+    color_one = ''
+    color_two = ''
+    color_three = ''
+    color_four = ''
+
+    color_one = input('Color 1: ')
+    while color_one not in colors:
+        print('Colors you have to chose from: blue, green, orange, purple, red, yellow')
+        color_one = input('Color 1: ')
+    color_two = input('Color 2: ')
+    while color_two not in colors:
+        print('Colors you have to chose from: blue, green, orange, purple, red, yellow')
+        color_two = input('Color 2: ')
+    color_three = input('Color 3: ')
+    while color_three not in colors:
+        print('Colors you have to chose from: blue, green, orange, purple, red, yellow')
+        color_three = input('Color 3: ')
+    color_four = input('Color 4: ')
+    while color_four not in colors:
+        print('Colors you have to chose from: blue, green, orange, purple, red, yellow')
+        color_four = input('Color 4: ')
+
+    answer_game.append(color_one)
+    answer_game.append(color_two)
+    answer_game.append(color_three)
+    answer_game.append(color_four)
+
+    return answer_game
+
+def pc_question(question_list, answer_code):
 
     '''
     input: a list wich consists of four colors
@@ -57,3 +95,25 @@ def human_question(question_list, answer_code):
             'niks'
 
     return sorted(list_check)
+
+def npc_question(question_list, code):
+    '''
+    input: a list wich consists of four colors
+           the secret code
+
+    returns: a list of
+             white_pins: correct GIVEN colors
+             black_pins: correct PLACED colors
+             in numbers
+    '''
+
+    white, black = 0, 0
+
+    for pin in pc_question(question_list, code):
+        if pin == 'white':
+            white += 1
+        else:
+            black += 1
+
+    return [str(black), str(white)]
+
--- Speelveld.py
+++ Speelveld.py
@@ -1,11 +1,5 @@
-# Wanneer het spel start kun je gaan raden
-# wanneer je wilt stoppen met het spel met einde spel ga je terug naar het startscherm
-# press start to begin voor start spel
-# met opslaan van speelveld worden alle oudere gokken opgeslagen en weergegeven
-# met update speelveld wordt het speelveld aangepast
-# met geheime code kan je de code weergeven als hij is geraden
-
 from Inters import *
+from Strats import *
 
 def save_playfield(list_question, list_pin):
 
@@ -30,21 +24,108 @@ def save_playfield(list_question, list_pin):
 
 def end():
     '''
-    Ends the game
+    Ends the game and asks if the player wants to go for another round
     '''
 
-def start():
+    print('Would you like to play again? yes/no')
+    answer = input('')
+
+    if answer == 'yes':
+        print('You can either choose to,\n'
+              'guess the computer\'s code or let the computer guess your code.\n'
+              '\n'
+              'type \'pc\' to guess yourself\n'
+              'type \'npc\' to let the computer guess\n')
+
+        version = input('Answer: ')
+
+        if version == 'pc':
+            start_pc()
+        elif version == 'npc':
+            start_npc()
+    else:
+        exit()
+
+def start_npc():
     '''
-    Starts the game
+    Starts the non-player controlled game
+    '''
+
+    global simplestrategy
+
+    print('Wich algorithm would you like to use?\n'
+          'simple strategy, worst case or fabian\n')
+
+    algo = input('Make your choice: ')
+
+    # Generates all possible guesses and puts them in a list
+    colors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
+    # Bron(Itertools): docent
+    guesses = list(itertools.product(colors, repeat=4))
+    print(len(guesses))
+
+    answer_game = answer_npc()
+
+    # Keeps track of all guesses
+    guess_counter = 0
+    # Picks a random code from all possible guesses left
+    current_guess = guesses[random.randint(0, len(guesses))]
+
+    # Asks the user for black and white pins
+    print(f'code : {current_guess}')
+    black_pins = input('How many colors are correctly placed?: ')
+    white_pins = input('How many colors are correctly given but not correctly placed?: ')
+
+    # The computer has guessed correctly, and will display it
+    if black_pins == 4:
+        print(f'You gave the code: {answer_game}.'
+              f'The computer guessed: {current_guess}'
+              f'The computer guessed correctly!')
+
+    # The guesses variable gets replaced by the new list, wich contains all the new possibilities
+    # if algo == 'simple strategy':
+    guesses = simplestrategy(guesses, current_guess, npc_question(current_guess, answer_game))
+
+    # The computer has 10 tries, once that is reached the computer has lost
+    while guess_counter != 11:
+        # Picks a random code from all possible guesses left
+        print(guesses)
+        if algo == 'simple strategy':
+            current_guess = guesses[random.randint(0, len(guesses))]
+        elif algo == 'worst case':
+            current_guess = worstcase(guesses, current_guess, [black_pins, white_pins])
+
+        # Asks the user for black and white pins
+        print(f'code : {current_guess}')
+
+        black_pins = input('How many colors are correctly placed?: ')
+        white_pins = input('How many colors are correctly given but not correctly placed?: ')
+
+        # The computer has guessed correctly, and will display it
+        if black_pins == '4':
+            print(f'You gave the code: {answer_game}.'
+                  f'The computer guessed: {current_guess}'
+                  f'The computer guessed correctly!')
+
+            end()
+
+        # The guesses variable gets replaced by the new list, wich contains all the new possibilities
+        # if algo == 'simple strategy':
+        guesses = simplestrategy(guesses, current_guess, npc_question(current_guess, answer_game))
+        print(len(guesses))
+
+def start_pc():
+    '''
+    Starts the player controlled game
     '''
 
     global answer
-    global human_question
+    global pc_question
 
     # Keeps track of the amount of questions the user has asked in total
     guess_counter = 0
     # Establishes the answer that the user needs to guess
-    answer = answer()
+    answer_game = answer()
     # Keeps track of past questions and pins and gives it to save_playfield()
     list_save_playfield_question = []
     list_save_playfield_pin = []
@@ -52,42 +133,62 @@ def start():
 
     # The colors the user can choose from
     colors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
-    print('Colors to chose from: blue, green, orange, purple, red, yellow\n'
+    print('Colors to choose from: blue, green, orange, purple, red, yellow\n'
           'Type \'start\' to start a new round')
 
     # The user has 10 guesses in total, once that is reached the while loop stops
-    while guess_counter != 10:
+    while guess_counter != 11:
 
-        list_human_question = []
+        list_pc_question = []
 
         # Each question takes four colors, once that is reached the while loops stops
-        while len(list_human_question) != 4:
+        while len(list_pc_question) != 4:
 
             guess = input('Guess a color: ')
 
             if guess == 'start':
-                start()
+                start_pc()
             # If the given color is incorrect, it keeps asking for a correct one
             while guess not in colors:
                 print('Colors you have to chose from: blue, green, orange, purple, red, yellow')
                 guess = input('Guess a color: ')
 
                 if guess == 'start':
-                    start()
+                    start_pc()
 
-            list_human_question.append(guess)
+            list_pc_question.append(guess)
 
         # Checks if the question is correct
-        if list_human_question == answer:
+        if list_pc_question == answer_game:
             print('Congrats! That\'s the right answer.')
             break
 
         # Incorrect questions will be checked for white and black pins
-        guess_pins = human_question(list_human_question, answer)
+        guess_pins = pc_question(list_pc_question, answer_game)
 
-        list_save_playfield_question.append(list_human_question)
+        list_save_playfield_question.append(list_pc_question)
         list_save_playfield_pin.append(guess_pins)
 
         print(save_playfield(list_save_playfield_question, list_save_playfield_pin))
 
-start()
+        guess_counter += 1
+
+    print('Oh no! You\'re a disappointing failure!')
+
+    end()
+
+# Introduction + giving the option to guess or give feedback
+print('Welcome to Fabian\'s version of Mastermind!\n'
+      'You can either choose to,\n'
+      'guess the computer\'s code or let the computer guess your code.\n'
+      '\n'
+      'type \'pc\' to guess yourself\n'
+      'type \'npc\' to let the computer guess\n')
+
+version = input('Make your choice: ')
+
+# obviously pc stands for player controlled and npc stands for non player controlled
+if version == 'pc':
+    start_pc()
+elif version == 'npc':
+    start_npc()
